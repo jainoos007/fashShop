b08b7911107a2fc9d862477afbe043b5e3ec7c26
jainoos <ammjainoos@gmail.com>
2025-01-02 02:11:26 +0530

    create lower nav bar and dropdown feature

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,10 +1,54 @@
 import React from "react";
 import logo from "../../assets/logo.png";
 import { IoMdSearch } from "react-icons/io";
-import { FaCartShopping } from "react-icons/fa6";
+import { FaCaretDown, FaCartShopping } from "react-icons/fa6";
 import DarkMode from "./darkMode";
 
 const Navbar = () => {
+  const Menu = [
+    {
+      id: 1,
+      name: "Home",
+      link: "#",
+    },
+    {
+      id: 2,
+      name: "Top Rated",
+      link: "#",
+    },
+    {
+      id: 3,
+      name: "Kids Wear",
+      link: "#",
+    },
+    {
+      id: 4,
+      name: "Mebs Wear",
+      link: "#",
+    },
+    {
+      id: 5,
+      name: "Electronics",
+      link: "#",
+    },
+  ];
+  const DropdownLinks = [
+    {
+      id: 1,
+      name: "Trending Products",
+      link: "#",
+    },
+    {
+      id: 2,
+      name: "Best Selling",
+      link: "#",
+    },
+    {
+      id: 1,
+      name: "Top Rated",
+      link: "#",
+    },
+  ];
   return (
     <div className="shadow-md bg-white dark:bg-gray-900 dark:text-white duration-200 relative z-40">
       {/* uppper nav bar */}
@@ -21,7 +65,7 @@ const Navbar = () => {
               <input
                 type="text"
                 placeholder="search"
-                className="w-[200px] sm:w-[200px] group-hover:w-[300px] transition-all duration-300 rounded-full border border-gray-300 px-2 py-1 focus:outline-none focus:border-1 focus:border-orange-400 focus:border-primary"
+                className="w-[200px] sm:w-[200px] group-hover:w-[300px] transition-all duration-300 rounded-full border border-gray-300 px-2 py-1 focus:outline-none focus:border-1 focus:border-primary dark:border-gray-500 dark:bg-gray-800"
               />
               <IoMdSearch className="text-gray-500 group-hover:text-primary absolute top-1/2 -translate-y-1/2 right-3" />
             </div>
@@ -45,7 +89,43 @@ const Navbar = () => {
         </div>
       </div>
       {/* lower nav bar */}
-      <div></div>
+      <div className="flex justify-center">
+        <ul className="sm:flex hidden items-center gap-4">
+          {Menu.map((data) => (
+            <li key={data.id}>
+              <a
+                href={data.link}
+                className="inline-block px-4 hover:text-primary duration-200"
+              >
+                {data.name}
+              </a>
+            </li>
+          ))}
+          {/* simple dropdown and links */}
+          <li className="group relative cursor-pointer">
+            <a href="#" className="flex items-center gap-[2px] py-2">
+              Trending Products
+              <span>
+                <FaCaretDown className="transition-all duration-200 hover:rotate-180" />
+              </span>
+            </a>
+            <div className="absolute z-[9999] hidden group-hover:block w-[150px] rounded-md bg-white p-2 text-black shadow-md">
+              <ul>
+                {DropdownLinks.map((data) => (
+                  <li key={data.id}>
+                    <a
+                      href={data.link}
+                      className="inline-block w-full rounded-md p-2 hover:bg-primary/20"
+                    >
+                      {data.name}
+                    </a>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 };
