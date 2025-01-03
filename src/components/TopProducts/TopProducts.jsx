fb24b45e66c8f13531db103c18b086c7a36538b6
jainoos <ammjainoos@gmail.com>
2025-01-03 22:20:49 +0530

    create top products component and adding aos animation

---
+++ src/components/TopProducts/TopProducts.jsx
@@ -0,0 +1,92 @@
+import React from "react";
+import Img1 from "../../assets/shirt/shirt.png";
+import Img2 from "../../assets/shirt/shirt2.png";
+import Img3 from "../../assets/shirt/shirt3.png";
+import { FaStar } from "react-icons/fa";
+
+const ProductData = [
+  {
+    id: 1,
+    img: Img1,
+    title: "Casual wear",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus aut quam deleniti quae impedit ullam saepe aspernatur sequi voluptate dolorem.",
+  },
+  {
+    id: 2,
+    img: Img2,
+    title: "Printed Shirt",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus aut quam deleniti quae impedit ullam saepe aspernatur sequi voluptate dolorem.",
+  },
+  {
+    id: 3,
+    img: Img3,
+    title: "women Shirt",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus aut quam deleniti quae impedit ullam saepe aspernatur sequi voluptate dolorem.",
+  },
+];
+
+const TopProducts = () => {
+  return (
+    <div>
+      <div className="container">
+        {/* Header section */}
+        <div className="text-left mb-24 maz-w-[600px]">
+          <p data-aos="fade-up" className="text-sm text-primary">
+            Top Rated Products for you
+          </p>
+          <h1 data-aos="fade-up" className="text-3xl font-bold">
+            Best Products
+          </h1>
+          <p data-aos="fade-up" className="text-xs text-gray-400">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae,
+            labore modi Sit asperiores modi!
+          </p>
+        </div>
+        {/* Body section */}
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-20 md:gap-5 place-items-center ">
+          {ProductData.map((data) => (
+            <div
+              data-aos="zoom-in"
+              key={data.id}
+              className="rounded-2xl bg-white dark:bg-gray-800 hover:bg-black/80 dark:hover:bg-primary hover:text-white relative shadow-xl duration-300 group max-w-[300px]"
+            >
+              {/* image section */}
+              <div className="h-[100px]">
+                <img
+                  src={data.img}
+                  alt=""
+                  className="max-w-[140px] block mx-auto transform -translate-y-20 group-hover:scale-105 duration-300 drop-shadow-md"
+                />
+              </div>
+              {/* details section */}
+              <div className="p-4 text-center">
+                {/* start rating */}
+                <div className="w-full flex items-center justify-center gap-1">
+                  <FaStar className="text-yellow-500" />
+                  <FaStar className="text-yellow-500" />
+                  <FaStar className="text-yellow-500" />
+                  <FaStar className="text-yellow-500" />
+                </div>
+                <h1 className="text-xl font-bold">{data.title}</h1>
+                <p className="text-gray-500 group-hover:text-white duration-300 text-sm line-clamp-2">
+                  {data.description}
+                </p>
+                <button
+                  className="bg-primary hover:scale-105 duration-300 text-white py-1 px-4 rounded-full mt-4 group-hover:bg-white group-hover:text-primary"
+                  // onClick={handleOrderPopup}
+                >
+                  Order now
+                </button>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default TopProducts;
